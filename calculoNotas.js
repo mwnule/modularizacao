bace0330a1ac03e579874accb5bbc60634c43611
mwnule <emanueledeoliveirasilveira@gmail.com>
2024-07-03 19:04:56 -0300

    primeiro exercicio completo

--- calculoNotas.js
+++ calculoNotas.js
@@ -8,7 +8,7 @@
 // 1. Solicitar ao usuário a quantidade de notas a serem calculadas
 //      - O que fazer com essa informação? Devo torna-lá dinâmica?
 // 2. Solicitar notas 
-//      - Fazer uma verificação de if e else para saber se a quantidade de notas com as notas solicitadas batem
+//      - Fazer uma verificação de if e else para saber se a quantidade de notas com as notas solicitadas batem (Não consegui fazer isso, mas planejo fazer depois)
 // 3. Criar um array com as notas
 // 4. Calcular média de forma dinâmica
 //      - Para isso devo usar função, então revise como funcionam
@@ -25,15 +25,31 @@ let quantidadeNotas = parseInt(quantidadeNotasStr)
 
 // Solicita ao usuário as notas, separadas por espaços para que sejam separadas por índice, e as armazebna em um array
 let notasInformadas = prompt("Pode informar quais são as notas, separando-as por espaços? ")
-let notas = notasInformadas.split(' ')
+let notas = notasInformadas.split(' ').map(Number)
+// .map(Number) é essencial para que transforme o resultado armazenado em número, pois prompt converte a informação dada do usuário automaticamnet em string
 
-function calculoTodasNotas ()
+// Função para calcular a soma de todas as notas, o que será essencial para fazer a média posteriormente
+function calculoTodasNotas(notas) {
+    let calculo = 0;
 
-function calcularMedia(notasInformadas, quantidadeNotas) {
-
+    for (let i = 0; i < notas.length; i++) {
+        calculo += notas[i]
+    }
+    return calculo
 }
+// Chama a função calculoTodasNotas e transforma seu resultado em uma variável
+let somaDasNotas = calculoTodasNotas(notas)
+
+// função criada para o calculo de média
+// ps: sempre colocar return para o código dar certo!
+function calcularMedia(somaDasNotas, quantidadeNotas) {
+    return somaDasNotas / quantidadeNotas
+}
+// Chama a função calcularMedia e transforma seu resultado em uma variável
+let mediaTotal = calcularMedia(somaDasNotas, quantidadeNotas)
 
 
+console.log(`A quantidade de notas é: ${quantidadeNotas}`)
+console.log(`As notas informadas são: ${notasInformadas}`)
+console.log(`A média total é: ${mediaTotal}`)
 
-console.log(quantidadeNotas)
-console.log(notas)
